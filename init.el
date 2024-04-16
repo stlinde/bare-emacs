d51a0504103f06de76105bfc463cca6491a5a41c
Sebastian Tved Linde <stl@yco.dk>
2024-04-16 20:53:43 +0200

    catchup commit

--- init.el
+++ init.el
@@ -2,6 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Packaging Settings
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Package Repositories
 (require 'package)
 (add-to-list 'package-archives '( "melpa" . "https://melpa.org/packages/") t)
@@ -10,6 +17,16 @@
   (unless (package-installed-p package)
     (package-install package))
   (require package))
+
+;; Place code inside lisp and site-lisp for loading modules
+(add-to-list 'load-path (concat user-emacs-directory "site-lisp/"))
+(add-to-list 'load-path (concat user-emacs-directory "lisp/"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   General Settings
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; No Littering
 (unless (package-installed-p 'no-littering)
@@ -20,14 +37,33 @@
 (require-package 'exec-path-from-shell)
 (exec-path-from-shell-initialize)
 
-;; Custom.el
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-
-;; Backups
 (setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups"))))
+(setopt auto-revert-avoid-polling t)  ;; Read from disk if file changes
+(setopt sentence-end-double-space nil)  ;; Fix archaic defaults
 
-;; UI
+(pixel-scroll-precision-mode)  ;; More precise scrolling
+
+;; Make right-click bring up menu instead of selecting text
+(when (display-graphic-p)
+  (context-menu-mode))
+
+;; WSL
+(defun wsl-copy-region-to-clipboard (start end)
+  "Copy region to Windows clipboard."
+  (interactive "r")
+  (call-process-region start end "clip.exe" nil 0))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   UI Settings
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; General
+
 ;; Theme
+(require-package 'modus-themes)
 (modus-themes-select 'modus-operandi-deuteranopia)
 
 (setq modus-themes-to-toggle '(modus-operandi-deuteranopia modus-vivendi-deuteranopia))
@@ -60,10 +96,14 @@
   (add-hook 'prog-mode-hook 'display-line-numbers-mode)
   (add-hook 'org-mode-hook 'display-line-numbers-mode))
 
-;; Minibuffer
-(fido-vertical-mode t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Development Settings
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Editing
+;; Compilation Buffer
+(add-hook 'compilation-filter-hook 'ansi-color-compilation-filter)
 
 ;; Electric Mode
 (add-hook 'after-init-hook #'electric-pair-mode)
@@ -75,16 +115,67 @@
 (require 'magit)
 (global-set-key (kbd "C-x g") #'magit-status)
 
+;; Terminal
+(require-package 'eat)
+
+;; Direnv
+(require-package 'envrc)
+(add-hook 'after-init-hook #'envrc-global-mode)
+
+;; Eglot
+(require-package 'eglot)
+
+;;; Hooks
+(add-hook 'python-ts-mode #'eglot-ensure)
+
+;; ;;; Bindings
+;; (global-set-key (kbd "C-c l l") #'eglot)
+
+;; (with-eval-after-load 'eglot
+;;   (global-set-key (kbd "C-c l c") #'eglot-reconnect)
+;;   (global-set-key (kbd "C-c l d") #'flymake-show-buffer-diagnostics)
+;;   (global-set-key (kbd "C-c l f f") #'eglot-format)
+;;   (global-set-key (kbd "C-c l f b") #'eglot-format-buffer)
+;;   (global-set-key (kbd "C-c l r n") #'eglot-rename)
+;;   (global-set-key (kbd "C-c l s") #'eglot-shutdown)
+;;   (global-set-key (kbd "C-c l i") #'eglot-inlay-hints-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Language Settings
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; C Programming
 (add-hook 'c-mode-hook 'c-ts-mode)
 (add-hook 'c-ts-mode-hook (lambda ()
 			    (setq-local c-ts-mode-indent-style 'linux)
 			    (setq-local c-ts-mode-indent-offset 8)))
 
-;; Terminal
-(require-package 'eat)
+;; Python Programming
+(add-hook 'python-mode-hook 'python-ts-mode)
 
-;; Org-mode
+;;; Setup Emacs-Pet
+(require-package 'f)
+(require 'pet)
+(add-hook 'python-base-mode-hook #'pet-mode -10)
+
+;;; Make emacs compilation buffer recognize pyright
+(with-eval-after-load 'compile
+  (add-to-list 'compilation-error-regexp-alist-alist
+               '(pyright "^[[:blank:]]+\\(.+\\):\\([0-9]+\\):\\([0-9]+\\).*$" 1 2 3))
+  (add-to-list 'compilation-error-regexp-alist 'pyright))
+
+(with-eval-after-load 'python-ts-mode
+  (defun rye-add (package)
+    (interactive)
+    (async-shell-command (concat "rye add " package))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Org Mode
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require-package 'org)
 
 ;; Keybindings
@@ -92,8 +183,9 @@
 (global-set-key (kbd "C-c c") #'org-capture)
 (global-set-key (kbd "C-c a") #'org-agenda)
 
-(setq org-directory "/home/slinde/org/"
-      org-default-notes-file (concat org-directory "inbox.org"))
+(setopt org-directory "/home/slinde/org/"
+	org-default-notes-file (concat org-directory "inbox.org")
+	org-agenda-files `(,org-directory))
 
 (setq org-capture-templates
       `(("t" "todo" entry (file "")  ; "" => `org-default-notes-file'
@@ -101,87 +193,64 @@
         ("n" "note" entry (file "")
          "* %? :NOTE:\n%U\n%a\n" :clock-resume t)))
 
-(setq browse-url-function 'eww-browse
-      shr-use-colors nil
-      shr-folding-mode t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Minibuffer/completion settings
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(global-set-key (kbd "C-c w") 'eww)
+;; Minibuffer
+(savehist-mode)  ;; Enable last used commands
 
-(require-package 'language-detection)
-(require 'cl-lib)
+;; Vertico
+(require-package 'vertico)
+(add-hook 'after-init-hook #'vertico-mode)
 
-(defun eww-tag-pre (dom)
-  (let ((shr-folding-mode 'none)
-        (shr-current-font 'default))
-    (shr-ensure-newline)
-    (insert (eww-fontify-pre dom))
-    (shr-ensure-newline)))
+;;; Hide commands in M-x that does not work in the current mode.
+(setopt read-extended-command-predicate #'command-completion-default-include-p)
 
-(defun eww-fontify-pre (dom)
-  (with-temp-buffer
-    (shr-generic dom)
-    (let ((mode (eww-buffer-auto-detect-mode)))
-      (when mode
-        (eww-fontify-buffer mode)))
-    (buffer-string)))
+;; Marginalia
+(require-package 'marginalia)
+(add-hook 'after-init-hook #'marginalia-mode)
 
-(defun eww-fontify-buffer (mode)
-  (delay-mode-hooks (funcall mode))
-  (font-lock-default-function mode)
-  (font-lock-default-fontify-region (point-min)
-                                    (point-max)
-                                    nil))
+;; Embark
+(require-package 'embark)
 
-(defun eww-buffer-auto-detect-mode ()
-  (let* ((map '((ada ada-mode)
-                (awk awk-mode)
-                (c c-mode)
-                (cpp c++-mode)
-                (clojure clojure-mode lisp-mode)
-                (csharp csharp-mode java-mode)
-                (css css-mode)
-                (dart dart-mode)
-                (delphi delphi-mode)
-                (emacslisp emacs-lisp-mode)
-                (erlang erlang-mode)
-                (fortran fortran-mode)
-                (fsharp fsharp-mode)
-                (go go-mode)
-                (groovy groovy-mode)
-                (haskell haskell-mode)
-                (html html-mode)
-                (java java-mode)
-                (javascript javascript-mode)
-                (json json-mode javascript-mode)
-                (latex latex-mode)
-                (lisp lisp-mode)
-                (lua lua-mode)
-                (matlab matlab-mode octave-mode)
-                (objc objc-mode c-mode)
-                (perl perl-mode)
-                (php php-mode)
-                (prolog prolog-mode)
-                (python python-mode)
-                (r r-mode)
-                (ruby ruby-mode)
-                (rust rust-mode)
-                (scala scala-mode)
-                (shell shell-script-mode)
-                (smalltalk smalltalk-mode)
-                (sql sql-mode)
-                (swift swift-mode)
-                (visualbasic visual-basic-mode)
-                (xml sgml-mode)))
-         (language (language-detection-string
-                    (buffer-substring-no-properties (point-min) (point-max))))
-         (modes (cdr (assoc language map)))
-         (mode (cl-loop for mode in modes
-                        when (fboundp mode)
-                        return mode)))
-    (message (format "%s" language))
-    (when (fboundp mode)
-      mode)))
+;; This functions as an alternative which-key.
+;; Instead of showing all the commands automatically, you hit C-h after to get
+;; a list of possible actions.
+(setq prefix-help-command #'embark-prefix-help-command)
+(global-set-key (kbd "C-.") #'embark-act)
+(global-set-key (kbd "M-.") #'embark-dwim)
+(global-set-key (kbd "C-h B") #'embark-bindings)
 
-(setq shr-external-rendering-functions
-      '((pre . eww-tag-pre)))
+;; Orderless
+(require-package 'orderless)
+(setopt completion-styles '(orderless basic)
+	completion-category-defaults nil
+	completion-category-overrides '((file (styles partial-completion))))
+
+;; Corfu
+(require-package 'corfu)
+(setopt corfu-auto t
+	corfu-cycle t
+	corfu-auto-delay 0.1
+	corfu-auto-prefix 1
+	corfu-popupinfo-delay '(0.5 . 0.25))
+(setopt tab-always-indent 'complete)
+(add-hook 'corfu-mode-hook #'corfu-popupinfo-mode)
+
+(add-hook 'prog-mode-hook #'corfu-mode)
+(add-hook 'org-mode-hook #'corfu-mode)
+
+(define-key corfu-map (kbd "RET") nil)
+(define-key corfu-map (kbd "C-y") #'corfu-insert)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Load
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'init-eww)
+
 ;;; init.el ends here
